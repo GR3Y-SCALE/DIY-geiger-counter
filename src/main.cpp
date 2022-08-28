9737a1004f161676b4d2e131b9f115a47b94a14c
GR3Y-SCALE <evanfoxx18@gmail.com>
2022-08-28 12:57:53 +1000

    started on new sample rate

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,44 @@
 #include <Arduino.h>
 #define LOG_PERIOD 5000
+#define CONVERSIONFACTOR 0.008120 //Specific for each GM tube
+#define NUM 3
 
 volatile unsigned long counts; //variable for GM Tube events
 unsigned long previousMillis; //variable for measuring time
 unsigned long currentMillis;
-volatile unsigned long totalCounts;
+unsigned long dose = 0;
+unsigned long countsPerMinute;
 
-
+unsigned long countArray[NUM];
+int arrayIndex;
 
 void impulse() { //calls on geiger event
 
 counts++;
-totalCounts++;
 
 }
 
+void initArray() {
+    for (int i = 0; i < NUM; i++) {
+        countArray[i] = 0;
+    }
+}
+
+int arrayAvg() {
+    int avgCounts = 0;
+    for (int i = 0; i < NUM; i++) {
+        avgCounts += countArray[i];
+    }
+    return avgCounts / NUM;
+}
+
+unsigned long ConvertToSieverts(long cpm = 0) {
+    return cpm*CONVERSIONFACTOR;
+}
+
+int scale(int val) {
+    return val*4;
+}
 
 void setup() { //setup
 
@@ -27,7 +51,10 @@ pinMode(2, INPUT);
 attachInterrupt(digitalPinToInterrupt(2), impulse, FALLING); //define external interrupts
 
 Serial.println("Start counter");
-totalCounts = 0;
+
+initArray();
+
+arrayIndex = 0;
 
 }
 
@@ -37,12 +64,22 @@ currentMillis = millis();
 
 if (currentMillis - previousMillis > LOG_PERIOD) {
 
-previousMillis = currentMillis;
+    previousMillis = currentMillis;
 
-Serial.print("Counts :"); Serial.print(counts); Serial.print(" Total Counts : "); Serial.println(totalCounts);
+    if (arrayIndex != 3) {
+        countArray[arrayIndex] = counts;
+        arrayIndex++;
+    } else {
+        arrayIndex = 0;
+        countArray[arrayIndex] = counts;
+    } 
+    countsPerMinute = scale(arrayAvg());
 
-counts = 0;
+    Serial.print("Counts Per Minute: "); Serial.print(countsPerMinute); Serial.print(" Seiverts: "); Serial.println(ConvertToSieverts(countsPerMinute)); Serial.print("index: "); Serial.println(counts);
+    counts = 0;
+}
+
+
 
 }
 
-}
